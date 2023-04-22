5af7536d3d4ff24f9f53c53d7c3df573eba76198
Shubham Prajapati <61042463+shubhamprajapati241@users.noreply.github.com>
2023-04-22 13:19:19 +0530

    commented in client.js

--- client/client.js
+++ client/client.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const serverUrl = "http://localhost:1225";
 
 async function main() {
+  // sending post request
   const greeting = await axios.post(`${serverUrl}/greet`, { name: "Shubham" });
   //   console.log(greeting.data);
   console.log("greeting from the server : " + JSON.stringify(greeting.data));
